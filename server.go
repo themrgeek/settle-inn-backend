5112697122336237dd1073eb1ced89bc23a95a25
Kathan Parekh <parekhkathan5@gmail.com>
2025-05-10 12:04:40 -0400

    final_commit

--- server.go
+++ server.go
@@ -2,15 +2,26 @@
 package main
 
 import (
-	"github.com/themrgeek/settleinn-backend/config"
-	"github.com/themrgeek/settleinn-backend/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/themrgeek/settleinn-backend/config"
+	"github.com/themrgeek/settleinn-backend/routes"
 )
 
 func main() {
 	config.ConnectDB()
 	r := gin.Default()
 	routes.SetupRoutes(r)
-	r.Run(":8080")
+
+	// Try running with SSL/HTTPS first
+	err := r.RunTLS(":443", "./certs/server.crt", "./certs/server.key")
+	if err != nil {
+		log.Printf("Failed to start HTTPS server: %v\nFalling back to HTTP on port 8080", err)
+		// Fallback to HTTP on port 8080
+		err = r.Run(":8080")
+		if err != nil {
+			log.Fatal("Failed to start HTTP server: ", err)
+		}
+	}
 }
